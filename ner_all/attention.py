ca0ceee1d29f9a4f5cf4a2cb1f92bc995eeaf1d9
evilbear <32738283+evilbear@users.noreply.github.com>
2018-07-24 16:13:48 +0800

    Chapter-level self-attention and multi-attention
    Chapter-level self-attention and multi-attention

---
+++ ner_all/attention.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+
+def Mask(inputs, seq_len, state=False, mode='mul'):
+    if seq_len == None:
+        return inputs
+    else:
+        mask = tf.cast(tf.sequence_mask(seq_len), tf.float32)
+        mask = tf.reshape(mask, (-1, tf.shape(mask)[0]*tf.shape(mask)[1]))
+        if state:
+            doc2vec_state = tf.constant([[1.]], tf.float32)
+            mask = tf.concat([mask,doc2vec_state],-1)
+        for _ in range(len(inputs.shape)-2):
+            mask = tf.expand_dims(mask, 2)
+        if mode == 'mul':
+            return inputs * mask
+        if mode == 'add':
+            return inputs - (1 - mask) * 1e12
+
+def Dense(inputs, output_size, bias=True, state=False, seq_len=None):
+    input_size = 600
+    W = tf.Variable(tf.random_uniform([input_size, output_size], -0.05, 0.05))
+    if bias:
+        b = tf.Variable(tf.random_uniform([output_size], -0.05, 0.05))
+    else:
+        b = 0
+    outputs = tf.matmul(tf.reshape(inputs, (-1, input_size)), W) + b
+    outputs = tf.reshape(outputs, tf.concat([tf.shape(inputs)[:-1], [output_size]], 0))
+    if seq_len != None:
+        outputs = Mask(outputs, seq_len, state, 'mul')
+    return outputs
+
+def self_attention(inputs, size, seq_len=None, doc2vec=None, state=False):
+    inputs_shape = tf.shape(inputs)
+    inputs = tf.reshape(inputs, (-1, inputs_shape[0]*inputs_shape[1], inputs_shape[2]))
+    if state:
+        doc2vec = tf.expand_dims(tf.expand_dims(doc2vec, 0), 0)
+        inputs = tf.concat([inputs,doc2vec],1)
+    combined = Dense(inputs, 3*size, False, state)
+    Q, K, V = tf.split(combined, [size, size, size], axis=-1)
+    A = tf.matmul(Q, K, transpose_b=True) / tf.sqrt(float(size))
+    A = tf.transpose(A, [0, 2, 1])
+    A = Mask(A, seq_len, state, mode='add')
+    A = tf.transpose(A, [0, 2, 1])
+    A = tf.nn.softmax(A)
+    O = tf.matmul(A, V)
+    O = Mask(O, seq_len, state, 'mul')
+    if state:
+        O = O[:,:-1,:]
+    O = tf.reshape(O, (-1, inputs_shape[1], size))
+    return O
+
+def multi_attention(inputs, heads, size_per_head, seq_len=None, doc2vec=None, state=False):
+    inputs_shape = tf.shape(inputs)
+    inputs = tf.reshape(inputs, (-1, inputs_shape[0]*inputs_shape[1], inputs_shape[2]))
+    if state:
+        doc2vec = tf.expand_dims(tf.expand_dims(doc2vec, 0), 0)
+        inputs = tf.concat([inputs,doc2vec],1)
+    combined = Dense(inputs, 3*heads*size_per_head, False, state)
+    Q, K, V = tf.split(combined, [heads*size_per_head, heads*size_per_head, heads*size_per_head], axis=-1)
+    Q = tf.reshape(Q, (-1, tf.shape(Q)[1], heads, size_per_head))
+    Q = tf.transpose(Q, [0, 2, 1, 3])
+    K = tf.reshape(K, (-1, tf.shape(K)[1], heads, size_per_head))
+    K = tf.transpose(K, [0, 2, 1, 3])
+    V = tf.reshape(V, (-1, tf.shape(V)[1], heads, size_per_head))
+    V = tf.transpose(V, [0, 2, 1, 3])
+    A = tf.matmul(Q, K, transpose_b=True) / tf.sqrt(float(size_per_head))
+    A = tf.transpose(A, [0, 3, 2, 1])
+    A = Mask(A, seq_len, state, mode='add')
+    A = tf.transpose(A, [0, 3, 2, 1])
+    A = tf.nn.softmax(A)
+    O = tf.matmul(A, V)
+    O = tf.transpose(O, [0, 2, 1, 3])
+    O = tf.reshape(O, (-1, tf.shape(O)[1], heads * size_per_head))
+    O = Mask(O, seq_len, state, 'mul')
+    if state:
+        O = O[:,:-1,:]
+    O = tf.reshape(O, (-1, inputs_shape[1], heads * size_per_head))
+    return O
---
+++ ner_all/ner_utils.py
@@ -0,0 +1,106 @@
+import os, argparse, codecs, logging
+import numpy as np
+
+def str2bool(v):
+    # copy from StackOverflow
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def get_logger(filename):
+    logger = logging.getLogger('logger')
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+    handler = logging.FileHandler(filename)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+    logging.getLogger().addHandler(handler)
+    return logger
+
+def get_vocab(filename):
+    vocab = dict()
+    with codecs.open(filename) as f:
+        for idx, word in enumerate(f):
+            word = word.strip('\n')
+            vocab[word] = idx
+    return vocab 
+
+def get_corpus(filename, vocab_words, vocab_tags):
+    with codecs.open(filename) as f:
+        words, tags, corpus, words_temp, tags_temp = [], [], [], [], []
+        for line in f:
+            line = line.strip('\n')
+            if len(line) == 0:
+                corpus.append([words, tags])
+                words, tags = [], []
+            else:
+                ls = line.split(' ')
+                word, tag = ls[0], ls[-1]
+                if word.isdigit():
+                    word = vocab_words['$NUM$']
+                elif word == '':
+                    word = vocab_words[" "]
+                elif word in vocab_words:
+                    word = vocab_words[word]
+                else:
+                    word = vocab_words['$UNK$']
+                tag = vocab_tags[tag]
+                words_temp += [word]
+                tags_temp += [tag]
+                if ls[0] == '。':
+                    words.append(words_temp)
+                    tags.append(tags_temp)
+                    words_temp, tags_temp = [], []
+    return corpus
+
+def pad_sequences(seqs):
+    max_length = max(map(lambda x: len(x), seqs))
+    sequence_padded, sequence_length = [], []
+    for seq in seqs:
+        seq = list(seq)
+        seq_ = seq[:max_length] + [0]*max(max_length - len(seq), 0)
+        sequence_padded.append(seq_)
+        sequence_length.append(min(len(seq), max_length))
+    return sequence_padded, sequence_length
+
+def get_tag(str1):
+    if str1 == 1:
+        return('B-AS')
+    elif str1 == 2:
+        return('I-AS')
+    elif str1 == 3:
+        return('B-SYM')
+    elif str1 == 4:
+        return('I-SYM')
+    elif str1 == 5:
+        return('B-ISYM')
+    elif str1 == 6:
+        return('I-ISYM')
+    elif str1 == 7:
+        return('B-DRUG')
+    elif str1 == 8:
+        return('I-DRUG')
+    elif str1 == 9:
+        return('B-SUR')
+    elif str1 == 10:
+        return('I-SUR')
+    else:
+        return('O')
+
+def get_doc2vec(filename):
+    with codecs.open(filename) as f:
+        corpus, words = [], []
+        for line in f:
+            line = line.strip('\n')
+            if len(line) == 0:
+                corpus.append(words)
+                words = []
+            else:
+                word = line.split(' ')[0]
+                if word == '':
+                    word = ' '
+                words += [word]
+    return corpus
